f5ef681da27e14acb59a49d1c0d48f93c1404938
Alfredo <alfredo.walter1980@gmail.com>
2017-11-16 11:10:56 -0300

    Initial Commit

---
+++ FAMovies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FAMovies
+//
+//  Created by Freddy on 16/11/17.
+//  Copyright © 2017 AlfredoAlonso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
